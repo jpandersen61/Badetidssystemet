adc450c8a5e86349a848a64cd7947ab7beecd18b
Zealand-Jean <55512813+Zealand-Jean@users.noreply.github.com>
2022-06-11 13:56:54 +0200

    Opgave 1

--- Badetidssystemet/Kreds.cs
+++ Badetidssystemet/Kreds.cs
@@ -6,5 +6,35 @@ namespace Badetidssystemet
 {
     class Kreds
     {
+        string _id;
+        string _navn;
+        string _adresse;
+        int _antalDeltagare;
+
+        public string ID { get { return _id; } private set { _id = value; } }
+        public string Navn { get { return _navn; } private set { _navn = value; } }
+        public string Adresse { get { return _adresse; } private set { _adresse = value; } }
+        public int AntalDeltagere { get { return _antalDeltagare; } private set { _antalDeltagare = value; } }
+
+        public Kreds()
+        {
+            ID = "";
+            Navn = "";
+            Adresse = "";
+            AntalDeltagere = 0;
+        }
+
+        public Kreds(string id, string navn, string adresse, int antalDeltagere)
+        {
+            ID = id;
+            Navn = navn;
+            Adresse = adresse;
+            AntalDeltagere = antalDeltagere;
+        }
+
+        public override string ToString()
+        {
+            return $"ID:{ID}, Navn:{Navn}, Adresse:{Adresse}, Deltagere:{AntalDeltagere}";
+        }
     }
 }
--- Badetidssystemet/Program.cs
+++ Badetidssystemet/Program.cs
@@ -6,7 +6,12 @@ namespace Badetidssystemet
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Opgave 1");
+            Kreds k1 = new Kreds("K1", "Roskilde", "Maglegaardsvej 2, 4000 Roskilde", 25);
+            Kreds k2 = new Kreds("K2", "København", "Rådhuspladsen 1, 1000 København", 100);
+            Console.WriteLine(k1);
+            Console.WriteLine(k2);
+
         }
     }
 }
